d31134b814822cef2a48346d22a5e4900a9ca819
David Malmström <david.malmstrm@gmail.com>
2019-06-27 11:46:30 +0200

    add quantity restriction on unseen tags in eval
    for performance reasons during training/validation

--- evaluate_recall.py
+++ evaluate_recall.py
@@ -18,24 +18,34 @@ def evaluate_model_recall(model, val_x, val_y, K):
     It is important that the val_x part is put before the rest of the data, when concatenated before training.
     This is because the precision functions rely on that the val_x part user numbers start with 0.
     """
-    y_pred = get_preds(model, val_x, K)
+    y_pred = get_preds(model, val_x, val_y, K)
     return recall_score(val_y, y_pred, average='micro'), jaccard_score(val_y, y_pred, average='micro')
 
 
-def get_preds(model, val_x, K):
+def get_preds(model, val_x, val_y, K):
     def top_cands(user_row, u_num):
         # Just the same user all the way, like in ncf (evaluate.py)
 
         # Make predictions only on the items that has not been added to the model 
         # (i.e. 1:s in the final concatenated train matrix)
-        not_seen_item_indices = np.where(np.squeeze(user_row.toarray()) == 0)[0]
-        users = np.full(len(not_seen_item_indices), u_num, dtype='int32')
-        predictions = model.predict([users, not_seen_item_indices],
+
+        # User a total of 100 different unseen tags to evaluate (using all causes too much computations)
+        num_unseen_tags = 100
+
+        all_true = user_row + val_y[u_num]
+        not_seen_tag_indices = np.where(np.squeeze(all_true.toarray()) == 0)[0]
+
+        tag_indices_to_predict = np.random.choice(not_seen_tag_indices, num_unseen_tags, replace=False)
+        tag_indices_to_predict = np.append(tag_indices_to_predict, np.nonzero(val_y[u_num])[1])
+        np.random.shuffle(tag_indices_to_predict)
+
+        users = np.full(len(tag_indices_to_predict), u_num, dtype='int32')
+        predictions = model.predict([users, tag_indices_to_predict],
                                     batch_size=100, verbose=0)
         
         map_item_score = {}
-        for i in range(len(not_seen_item_indices)):
-            item_index = not_seen_item_indices[i]
+        for i in range(len(tag_indices_to_predict)):
+            item_index = tag_indices_to_predict[i]
             map_item_score[item_index] = predictions[i][0]
     
         # return top rank list
