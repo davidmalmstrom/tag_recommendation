1287637a25d0d7f175e239ffaef2a6cf4f31c26f
David Malmström <david.malmstrm@gmail.com>
2019-07-16 12:25:51 +0200

    add ".yml" filter in run_several.py

--- base/run_several.py
+++ base/run_several.py
@@ -11,8 +11,9 @@ run_files = os.listdir("runs/several_runs/")
 run_files.sort()
 
 for run_file_name in run_files:
-    try:
-        run_script.main(["run_several.py", run_file_name], "runs/several_runs/")
-    except Exception as e:
-        print((str(type(e)) + ": " + str(e)).replace('\n', ' '))
+    if run_file_name[-4:] == ".yml":
+        try:
+            run_script.main(["run_several.py", run_file_name], "runs/several_runs/")
+        except Exception as e:
+            print((str(type(e)) + ": " + str(e)).replace('\n', ' '))
 #os.system("shutdown now -h")
