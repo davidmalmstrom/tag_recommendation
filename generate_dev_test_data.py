e4a6013f9f16628dea94500a72e5cfdbec017c11
David Malmström <david.malmstrm@gmail.com>
2019-07-24 11:59:21 +0200

    add cold-start data generation

--- generate_dev_test_data.py
+++ generate_dev_test_data.py
@@ -21,3 +21,21 @@ with open(path + 'test_tag_dataset.pkl', 'wb') as f:
     test_x, test_y = nh.split_user_tags_percentage(y[:2000])
     y = sp.vstack([test_x, y[2000:]]).todok()
     pickle.dump((X, y, mlbx, mlby, val_y, test_y), f)
+
+with open(path + 'cold_0.1_test_tag_dataset.pkl', 'wb') as f:
+    # first 2000 is test set and last 2000 is val set (for training the models for testing)
+    X, y, mlbx, mlby = nh.reshape_data(dataset)
+    val_x, val_y = nh.split_user_tags_percentage(y[-2000:], percentage=0.1)
+    y = sp.vstack([y[:-2000], val_x]).todok()
+    test_x, test_y = nh.split_user_tags_percentage(y[:2000], percentage=0.1)
+    y = sp.vstack([test_x, y[2000:]]).todok()
+    pickle.dump((X, y, mlbx, mlby, val_y, test_y), f)
+
+with open(path + 'cold_0.0_test_tag_dataset.pkl', 'wb') as f:
+    # first 2000 is test set and last 2000 is val set (for training the models for testing)
+    X, y, mlbx, mlby = nh.reshape_data(dataset)
+    val_x, val_y = nh.split_user_tags_percentage(y[-2000:], percentage=0)
+    y = sp.vstack([y[:-2000], val_x]).todok()
+    test_x, test_y = nh.split_user_tags_percentage(y[:2000], percentage=0)
+    y = sp.vstack([test_x, y[2000:]]).todok()
+    pickle.dump((X, y, mlbx, mlby, val_y, test_y), f)
