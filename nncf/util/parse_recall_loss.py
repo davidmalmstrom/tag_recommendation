73717df837a3fbcd07d2d22050f1e141f41dc700
David Malmström <david.malmstrm@gmail.com>
2019-09-17 12:34:10 +0200

    improve parse_recall_loss script

--- nncf/util/parse_recall_loss.py
+++ nncf/util/parse_recall_loss.py
@@ -10,20 +10,27 @@ def process_line(line):
     loss, _ = rest.split('loss = ')[1].split(',', 1)
     return [int(iteration), float(recall), float(loss)]
 
-with open(sys.argv[1]) as f:
+try:
+    runfile_path = sys.argv[1]
+    runfile_name = runfile_path.split('/')[-1]
+except IndexError:
+    print("Error: Runfile path required.")
+    raise
+
+with open(runfile_path) as f:
     data = [
         process_line(line)
         for line in f if '# Iteration ' in line
     ]
 
-df = pd.DataFrame(data, columns=['iteration', 'recall', 'loss'])
+df = pd.DataFrame(data, columns=['Iteration', 'Recall@M', 'Loss'])
 
 plt.figure()
-zxc = sns.lineplot(x="iteration", y="recall",
+zxc = sns.lineplot(x="Iteration", y="Recall@M",
              data=df)
-plt.savefig('recall_graph.svg', bbox_inches='tight')
+plt.savefig('recall_graph_{}.svg'.format(runfile_name), bbox_inches='tight')
 
 plt.figure()
-zxc = sns.lineplot(x="iteration", y="loss",
+zxc = sns.lineplot(x="Iteration", y="Loss",
              data=df)
-plt.savefig('loss_graph.svg', bbox_inches='tight')
+plt.savefig('loss_graph_{}.svg'.format(runfile_name), bbox_inches='tight')
