49a380f6cb006dd0840b3bdea18ddccb0e5527c7
David Malmström <david.malmstrm@gmail.com>
2019-09-30 12:37:32 +0200

    allow removal or addition of runs when runsev runs

--- nncf/run_several.py
+++ nncf/run_several.py
@@ -7,15 +7,26 @@ import os
 sys.path.append('..')
 import run_script
 
-run_files = os.listdir("runs/several_runs/")
-run_files.sort()
+files_path = "runs/several_runs/"
 
-for run_file_name in run_files:
-    try:
-        run_script.main(["run_several.py", run_file_name], "runs/several_runs/")
-    except Exception as e:
-        print((str(type(e)) + ": " + str(e)).replace('\n', ' '))
-        sys.stdout = sys.stdout.terminal  # Reset stdout so that new prints
-                                            # does not go into old file
+run_files = os.listdir(files_path)
+run_files.sort()
+seen = set()
+
+while seen != set(run_files):
+    for run_file_name in run_files:
+        if run_file_name not in seen:
+            try:
+                seen.add(run_file_name)
+                run_script.main(["run_several.py", run_file_name], "runs/several_runs/")
+            except Exception as e:
+                print((str(type(e)) + ": " + str(e)).replace('\n', ' '))
+                sys.stdout = sys.stdout.terminal  # Reset stdout so that new prints
+                                                    # does not go into old file
+            break
+
+    run_files = os.listdir(files_path)
+    run_files.sort()
+
 # os.system("shutdown now -h")  # run with sudo nohup /home/david/miniconda2/envs/t10/bin/python run_several.py
 # and with ctrl+Z and bg %1
